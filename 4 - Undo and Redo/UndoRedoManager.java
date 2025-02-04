fbf1b2b942f8713b050b9ca7575c3047c64d1998
Declan <declan.derible@keyin.com>
2025-02-04 14:16:49 -0330

    assignment 4 submission

--- 4 - Undo and Redo/UndoRedoManager.java
+++ 4 - Undo and Redo/UndoRedoManager.java
@@ -18,23 +18,53 @@ public class UndoRedoManager<T> {
     }
     private Node currentState;
     //Undo operation
-    public T undo(){
-        //implement me
+    public T undo() {
+            if (currentState == null || currentState.prev == null) {
+                return null;
+            }
+            currentState = currentState.prev;
+            return currentState.state;
+        }
     }
 
     //perform an operation
     public void  addState (T newState) {
-        //implement me
+        Node newNode = new Node(newState);
 
+        if (currentState == null) {
+            currentState = newNode;
+            return;
+        }
+        currentState.next = newNode;
+        newNode.prev = currentState;
+        currentState = newNode;
     }
 
     //Redo Operation
-    public T redo(){
-        //implement me
+    public T redo() {
+        if (currentState == null || currentState.next == null) {
+            return null;
+        }
+        currentState = currentState.next;
+        return currentState.state;
     }
 
     public static void main(String[] args) {
+        UndoRedoManager<String> undoRedoManager = new UndoRedoManager<>();
+        undoRedoManager.addstate("Winter");
+        undoRedoManager.addstate("Spring");
+        undoRedoManager.addstate("Summer");
+        undoRedoManager.addstate("Autumn");
 
-
+        System.out.println(undoRedoManager.undo()); // Summer
+        System.out.println(undoRedoManager.undo()); // Spring
+        System.out.println(undoRedoManager.redo()); // Summer
+        System.out.println(undoRedoManager.undo()); // Spring
+        System.out.println(undoRedoManager.undo()); // Winter
+        System.out.println(undoRedoManager.redo()); // Spring
+        System.out.println(undoRedoManager.redo()); // Summer
+        System.out.println(undoRedoManager.redo()); // Autumn
+        System.out.println(); // Blank
+        System.out.println(undoRedoManager.getCurrentState()); // Autumn
     }
 }
