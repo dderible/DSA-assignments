3329c7dda82e525df0b60cc2e800cef5500d594f
Declan <declan.derible@keyin.com>
2025-02-03 21:41:29 -0330

    finishing single linked list

--- 3 - Complete Linked List Delete Method/Node.java
+++ 3 - Complete Linked List Delete Method/Node.java
@@ -1,3 +1,4 @@
 public class Node {
-
+    public int value;
+    public Node next;
 }
--- 3 - Complete Linked List Delete Method/SingleLinkedList.java
+++ 3 - Complete Linked List Delete Method/SingleLinkedList.java
@@ -5,5 +5,121 @@
 //3. delete anywhere
 
 public class SingleLinkedList {
+    public Node head;
+    public Node tail;
+    public int size;
 
+    public Node createdLinkedList (int nodeValue) {
+        Node node = new Node();
+        node.next = null;
+        node.value = nodeValue;
+        head = node;
+        tail = node;
+        size = 1;
+        return head;
+    }
+
+    public void insertInLinkedList (int nodeValue, int location) {
+        Node node = new Node();
+        node.value = nodeValue;
+        if (head == null) {
+            createdLinkedList(nodeValue);
+            return;
+
+        } else if (location == 0) {
+            node.next = head;
+            head = node;
+
+        } else if (location >= size) {
+            tail.next = node;
+            node.next = null;
+            tail = node;
+
+        } else {
+            Node tempNode = head;
+            int index = 0;
+            while (index < location - 1) {
+                tempNode = tempNode.next;
+                index++;
+            }
+            Node nextNode = node;
+            node.next = nextNode;
+        }
+        size++;
+    }
+
+    public void traverseLinkedList () {
+        if (head == null) {
+            System.out.println("ERROR: List does not exist.");
+
+        } else {
+            Node temNode = head;
+            for (int i = 0; i < size; i++) {
+                System.out.print(temNode.value);
+                if(i != size -1) {
+                    System.out.print(" -> ");
+                }
+                temNode = temNode.next;
+            }
+            System.out.print("\n");
+        }
+    }
+
+    public boolean searchNode (int nodeValue) {
+        if (head != null) {
+            Node tempNode = head;
+            for (int i = 0; i < size; i++) {
+                if (tempNode.value == nodeValue) {
+                    System.out.println("SUCCESS: Found the node: " + tempNode.value + " at location: " + i + "\n");
+                    return true;
+                }
+                tempNode = tempNode.next;
+            }
+        }
+        System.out.println("ERROR: Node not found.");
+        return false;
+    }
+
+    public void deleteNode (int nodeValue) {
+        if (head != null) {
+            System.out.println("ERROR: List does not exist.");
+            return;
+
+        } else if (location == 0) {
+            head = head.next;
+            size--;
+            if (size == 0) {
+                tail = null;
+            }
+            System.out.println("SUCCESS: Deleted Node at: " + location + ".");
+            return;
+
+        } else if (location >= size - 1) {
+            Node tempNode = head;
+            for (int i = 0; i < size - 2; i++) {
+                tempNode = tempNode.next;
+            }
+            if (tempNode == head) {
+                head = null;
+                tail = null;
+                size--;
+                System.out.println("SUCCESS: Deleted Node at: " + location + ".");
+                return;
+            }
+            tempNode.next = null;
+            tail = tempNode;
+            size--;
+            System.out.println("SUCCESS: Deleted Node at: " + location + ".");
+            return;
+
+        } else {
+            Node tempNode = head;
+            for (int i = 0; i < location - 1; i++) {
+                tempNode = tempNode.next;
+            }
+            tempNode.next = tempNode.next.next;
+            size--;
+            System.out.println("SUCCESS: Deleted Node at: " + location + ".");
+        }
+    }
 }
